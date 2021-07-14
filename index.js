5d8c1a9be592a9fed21892ade1b14f19bfc24213
Sanoo Singh <vannguyen141290@gmail.com>
2021-07-14 10:43:24 -0700

    solved task 6

--- index.js
+++ index.js
@@ -134,9 +134,16 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
+function removeFlavorByName(array, flavor){
   /*your code here*/
+  for (let i=0; i<array.length; i++){
+    if (array[i] === flavor){
+      array.splice(i, 1);
+    }
+  }
+  return array;
 }
+console.log(removeFlavorByName(originalFlavors, 'Rocky Road'));
 
 
 
@@ -160,9 +167,17 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, string){
+  filteredArray =[];
+  for (i=0; i< array.length; i++){
+    if (array[i].includes(string)){
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
 }
+
+console.log(filterByWord(originalFlavors, 'Chocolate'));
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
